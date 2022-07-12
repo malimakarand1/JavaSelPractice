7c566b31bec64be0563a63e4d61f63153535a0f8
malimakarand1 <malimakarand1@gmail.com>
2022-07-12 17:18:31 +0530

    methods practice

---
+++ JavaSelPractice/JavaForSelenium/JavaPracticeForSelenium/MethodsPractice.java
@@ -0,0 +1,18 @@
+package JavaPracticeForSelenium;
+
+public class MethodsPractice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MethodsPractice a= new MethodsPractice();
+		String d=a.getData();
+		System.out.println(d);
+
+	}
+	
+	public String getData() {
+		System.out.println("Hello world");
+		return "hi i am mali mali makarand";
+	}
+
+}
---
+++ JavaSelPractice/JavaForSelenium/JavaPracticeForSelenium/MethodsPractice2.java
@@ -0,0 +1,12 @@
+package JavaPracticeForSelenium;
+
+public class MethodsPractice2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MethodsPractice MethodFromOtherClass= new MethodsPractice();
+		String data=MethodFromOtherClass.getData();
+		System.out.println(data);
+	}
+
+}
