7414edef60fa23151509ccc382f740e20d9aad48
Makarand Laptop <malimakarand1@gmail.com>
2022-07-19 20:32:07 +0530

    login forget pass word reset again login on rohitshetty site

--- JavaSelPractice/JavaForSelenium/SeleniumWithJavaPrograms/XPathYouTubeChallenge.java
+++ JavaSelPractice/JavaForSelenium/SeleniumWithJavaPrograms/XPathYouTubeChallenge.java
@@ -1,6 +1,7 @@
 package SeleniumWithJavaPrograms;
 
 import java.time.Duration;
+import java.util.ArrayList;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -8,17 +9,18 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class XPathYouTubeChallenge {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 		System.setProperty("webdriver.chrome.driver","C:\\mywebdrivers//chromedriver.exe");
 		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 		driver.get("https://www.youtube.com/");
-		driver.manage().window().maximize();
 		driver.findElement(By.xpath("/html/body/ytd-app/div[1]/ytd-page-manager/ytd-browse[1]/ytd-two-column-browse-results-renderer/div[1]/ytd-rich-grid-renderer/div[6]/ytd-rich-grid-row[1]/div/ytd-rich-item-renderer[2]/div/ytd-rich-grid-media/div[1]/div[2]/div[1]/h3/a/yt-formatted-string")).click();
-		String a=driver.findElement(By.xpath("//div[@id='upload-info']//a[@class='yt-simple-endpoint style-scope yt-formatted-string'][normalize-space()='Shemaroo Comedy']")).getAccessibleName();
+		String a=driver.findElement(By.cssSelector("yt-formatted-string[id='text'] a[class='yt-simple-endpoint style-scope yt-formatted-string']")).getText();
 		System.out.println(a);
 		driver.close();
+	
 
 	}
 
